043d17024b28b6d362c625ea82c207e74b8c58ff
Neha Gul <nehagul9933@gmail.com>
2026-05-02 18:23:14 +0500

    Initial MediBridge project setup

---
+++ src/test/java/com/medibridge/medibridge/MedibridgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medibridge.medibridge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedibridgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
